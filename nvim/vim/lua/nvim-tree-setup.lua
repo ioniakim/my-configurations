822047ee9488cd75b031dce848a0a225468b9603
S W Kim <ionia.kim@gmail.com>
2022-06-13 00:40:43 +0900

    Remove vim dir mistakenly brought back alive

--- nvim/vim/lua/nvim-tree-setup.lua
+++
@@ -1,8 +0,0 @@
-require 'nvim-tree'.setup{
-}
-
-local map = vim.keymap.set
-
-map('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true })
