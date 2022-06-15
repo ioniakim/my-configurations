8124d33bd1003e5d353345d4b399fd0ba5b67463
S W Kim <ionia.kim@gmail.com>
2022-06-16 01:25:28 +0900

    Add ensure_list to treesitter

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,4 +1,8 @@
 require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {"c", "lua", "python", "java", "rust", "bash", "json", "html", "toml", "vim", "yaml" },
+    -- Install parsers synchronously (only applied to ensure_installed)
+    sync_install = true,
     highlight = {
         enable = true,              -- false will disable the whole extension
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
