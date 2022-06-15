2eab8c1e8ee5b8fe3c69e111d39bcc1bcb0a4221
S W Kim <ionia.kim@gmail.com>
2022-06-16 00:04:52 +0900

    Move nvim-tree autocmd to autocmd.lua

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,4 +1,6 @@
+local autocmd = vim.api.nvim_create_autocmd
 
+-- packer.nvim
 -- Automatically run :PackerCompile whenever plugins.lua is updated with an autocommand
 vim.cmd([[
     augroup packer_user_config
@@ -6,3 +8,14 @@ vim.cmd([[
         autocmd BufWritePost plugins.lua source <afile> | PackerCompile
     augroup end
 ]])
+
+-- nvim-tree.nvim
+-- auto close on the case only nvim-tree window is left.
+autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nill then
+            vim.cmd "quit"
+        end
+    end
+})
--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,5 +1,3 @@
-local autocmd = vim.api.nvim_create_autocmd
-
 require 'nvim-tree'.setup{
 }
 
@@ -10,12 +8,3 @@ map('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
 map('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
 map('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true })
 
--- auto close
-autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nill then
-            vim.cmd "quit"
-        end
-    end
-})
