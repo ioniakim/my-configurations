8cbd17a6a860d3e1966e3f97a7e13f69aef1666a
S W Kim <ionia.kim@gmail.com>
2022-06-24 01:31:26 +0900

    Remove auto close on nvim-tree because it causes troubles

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -9,13 +9,3 @@ vim.cmd([[
     augroup end
 ]])
 
--- nvim-tree.nvim
--- auto close on the case only nvim-tree window is left.
-autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nill then
-            vim.cmd "quit"
-        end
-    end
-})
