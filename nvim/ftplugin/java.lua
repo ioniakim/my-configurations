818f1de91f760d2471c13115ee2893fc09570cca
S W Kim <ionia.kim@gmail.com>
2022-07-14 22:45:40 +0900

    Add keymap on java lsp

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -150,14 +150,29 @@ vim.cmd("command! -buffer JdtJshell lua require('jdtls').jshell()")
 
 local keymap = vim.keymap.set
 
-local opts = { silent = true }
+local opts = { silent = true, noremap = true }
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<CR>", opts)
+keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+keymap("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 
-keymap('n', '<A-o>', '<Cmd>lua require(\'jdtls\').organize_imports()<CR>', { noremap = true })
-keymap('n', 'crv', '<Cmd>lua require(\'jdtls\').extract_variable()<CR>', { noremap = true })
-keymap('v', 'crv', '<Esc><Cmd>lua require(\'jdtls\').extract_variable(true)<CR>', { noremap = true })
-keymap('n', 'crc', '<Cmd>lua require(\'jdtls\').extract_constant()<CR>', { noremap = true })
-keymap('v', 'crc', '<Esc><Cmd>lua require(\'jdtls\').extract_constant(true)<CR>', { noremap = true })
-keymap('v', 'crm', '<Esc><Cmd>lua require(\'jdtls\').extract_method(true)<CR>', { noremap = true })
+keymap('n', '<A-o>', '<Cmd>lua require(\'jdtls\').organize_imports()<CR>', opts)
+keymap('n', 'crv', '<Cmd>lua require(\'jdtls\').extract_variable()<CR>', opts)
+keymap('v', 'crv', '<Esc><Cmd>lua require(\'jdtls\').extract_variable(true)<CR>', opts)
+keymap('n', 'crc', '<Cmd>lua require(\'jdtls\').extract_constant()<CR>', opts)
+keymap('v', 'crc', '<Esc><Cmd>lua require(\'jdtls\').extract_constant(true)<CR>', opts)
+keymap('v', 'crm', '<Esc><Cmd>lua require(\'jdtls\').extract_method(true)<CR>', opts)
 
 
 -- If using nvim-dap
