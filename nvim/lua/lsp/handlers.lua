2eee3782d0a38dae17d3345ea4ba99224a9fd3c3
S W Kim <ionia.kim@gmail.com>
2022-08-13 02:06:16 +0900

    Add more lsp keymap

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -43,27 +43,37 @@ M.setup = function()
 end
 
 local function lsp_keymaps(bufnr)
-    local opts = { noremap = true, silent = true, buffer=bufnr }
-    -- local keymap = vim.api.nvim_buf_set_keymap
+    local opts = { noremap = true, silent = true, buffer = bufnr }
     local keymap = vim.keymap.set
     keymap("n", "gD", vim.lsp.buf.declaration, opts)
     keymap("n", "gd", vim.lsp.buf.definition, opts)
+    keymap("n", "<space>D", vim.lsp.buf.type_definition, opts)
     keymap("n", "K", vim.lsp.buf.hover, opts)
     keymap("n", "gi", vim.lsp.buf.implementation, opts)
     keymap("n", "gr", vim.lsp.buf.references, opts)
     keymap("n", "gl", vim.diagnostic.open_float, opts)
-    keymap("n", "<space>f", vim.lsp.buf.formatting, opts)
-    keymap("n", "<space>li", "<CMD>LspInfo<CR>", opts)
-    keymap("n", "<space>m", "<CMD>Mason<CR>", opts)
+    keymap("n", "<space>f", function()
+        vim.lsp.buf.format { async = true }
+    end, opts)
+    keymap("n", "<space>li", "<cmd>LspInfo<cr>", opts)
+    keymap("n", "<space>m", "<cmd>Mason<cr>", opts)
     keymap("n", "<space>ca", vim.lsp.buf.code_action, opts)
     keymap("n", "<space>gn", vim.diagnostic.goto_next, opts)
     keymap("n", "<space>gp", vim.diagnostic.goto_prev, opts)
     keymap("n", "<space>rn", vim.lsp.buf.rename, opts)
     keymap("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     keymap("n", "<space>sl", vim.diagnostic.setloclist, opts)
+    keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    keymap("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
 end
 
 M.on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
     local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
     if not status_cmp_ok then
         return
