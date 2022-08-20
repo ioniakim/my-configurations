18c060eb3e3906769d1a84d548a59200af2ad038
S W Kim <ionia.kim@gmail.com>
2022-08-20 14:24:32 +0900

    Make cmp and illuminate work

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -1,5 +1,16 @@
 local M = {}
 
+local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_cmp_ok then
+    return
+end
+
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+
+
 M.setup = function()
     local signs = {
 
@@ -52,17 +63,17 @@ local function lsp_keymaps(bufnr)
     keymap("n", "gi", vim.lsp.buf.implementation, opts)
     keymap("n", "gr", vim.lsp.buf.references, opts)
     keymap("n", "gl", vim.diagnostic.open_float, opts)
-    keymap("n", "<space>f", function()
+    keymap("n", "<space>lf", function()
         vim.lsp.buf.format { async = true }
     end, opts)
     keymap("n", "<space>li", "<cmd>LspInfo<cr>", opts)
-    keymap("n", "<space>m", "<cmd>Mason<cr>", opts)
-    keymap("n", "<space>ca", vim.lsp.buf.code_action, opts)
-    keymap("n", "<space>gn", vim.diagnostic.goto_next, opts)
-    keymap("n", "<space>gp", vim.diagnostic.goto_prev, opts)
-    keymap("n", "<space>rn", vim.lsp.buf.rename, opts)
-    keymap("n", "<space>h", vim.lsp.buf.signature_help, opts)
-    keymap("n", "<space>sl", vim.diagnostic.setloclist, opts)
+    keymap("n", "<space>lI", "<cmd>Mason<cr>", opts)
+    keymap("n", "<space>la", vim.lsp.buf.code_action, opts)
+    keymap("n", "<space>lj", vim.diagnostic.goto_next, opts)
+    keymap("n", "<space>lk", vim.diagnostic.goto_prev, opts)
+    keymap("n", "<space>lr", vim.lsp.buf.rename, opts)
+    keymap("n", "<space>lh", vim.lsp.buf.signature_help, opts)
+    keymap("n", "<space>lq", vim.diagnostic.setloclist, opts)
     keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
     keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
     keymap("n", "<space>wl", function()
@@ -74,17 +85,11 @@ M.on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-    if not status_cmp_ok then
-        return
+    if client.name == "sumneko_lua" then
+        client.server_capabilities.document_formatting = false
     end
 
     lsp_keymaps(bufnr)
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-        return
-    end
-    illuminate.on_attach(client)
 
     if client.name == "jdt.ls" then
         vim.lsp.codelens.refresh()
@@ -92,13 +97,10 @@ M.on_attach = function(client, bufnr)
             require("jdtls").setup_dap { hotcodereplace = "auto" }
             require("jdtls.dap").setup_dap_main_class_configs()
         end
-        client.resolved_capabilities.document_formatting = true
-        client.resolved_capabilities.textDocument.completion.completionItem.snippetSupport = false
+        client.server_capabilities.document_formatting = true
+        client.server_capabilities.textDocument.completion.completionItem.snippetSupport = false
     end
 
-    M.capabilities = vim.lsp.protocol.make_client_capabilities()
-    M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-    M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 end
 
 return M
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,45 +15,36 @@ if fn.empty(fn.glob(install_path)) > 0 then
         "--depth", "1",
         "https://github.com/wbthomason/packer.nvim",
         install_path })
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
-return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+]]
 
-    use {
-        "kyazdani42/nvim-tree.lua",
-        requires = {
-            "kyazdani42/nvim-web-devicons", -- optional, for file icon
-        },
-        tag = "nightly", -- optional, updated every week
-        config = get_config("nvim-tree")
-    }
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
 
-    use {
-        "nvim-telescope/telescope.nvim",
-        requires = { { "nvim-lua/plenary.nvim" } },
-        config = get_config("telescope")
-    }
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
+}
 
-    -- Highlighting
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        requires = {
-            "nvim-treesitter/nvim-treesitter-refactor",
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
-        config = get_config("treesitter"),
-        run = ":TSUpdate",
-    }
-
-    -- [[ Theme ]]
-    use { "mhinz/vim-startify" } -- start screen
-    use { "Mofiqul/dracula.nvim" } -- colorscheme
-    use {
-        "nvim-lualine/lualine.nvim", -- statusline
-        requires = { "kyazdani42/nvim-web-devicons", opt = true },
-        config = get_config("lualine"),
-    }
+return packer.startup(function(use)
+    use { "wbthomason/packer.nvim" } -- Have packer manage itself
+    use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
 
     -- Comment out
     -- gcc : comment out one line
@@ -66,25 +57,36 @@ return require("packer").startup(function(use)
 
     use { "tpope/vim-repeat" }
 
-    -- Terminal
-    use { "akinsho/toggleterm.nvim",
-        tag = "v2.*",
-        config = get_config("toggleterm")
+    use { "windwp/nvim-autopairs", commit = "fa6876f832ea1b71801c4e481d8feca9a36215ec" } -- Autopairs, integrates with both cmp and treesitter
+
+    use { "kyazdani42/nvim-web-devicons" } -- optional, for file icon
+
+    use {
+        "kyazdani42/nvim-tree.lua",
+        tag = "nightly", -- optional, updated every week
     }
+
+    use { "nvim-lualine/lualine.nvim" } -- statusline 
+    -- Terminal
+    use { "akinsho/toggleterm.nvim", tag = "v2.*" }
+
+    -- [[ Theme ]]
+    use { "mhinz/vim-startify" } -- start screen
+    use { "Mofiqul/dracula.nvim" } -- colorscheme
 
     -- cmp plugins
     -- The completion plugin
-    use {
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/cmp-buffer", -- buffer completions
-            "hrsh7th/cmp-path", -- path completions
-            "saadparwaiz1/cmp_luasnip", -- snippet completions
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-        },
-        config = get_config("cmp"),
-    }
+    use { "hrsh7th/nvim-cmp" }
+    use { "hrsh7th/cmp-buffer" } -- buffer completions
+    use { "hrsh7th/cmp-path" } -- path completions
+    use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
+    use { "hrsh7th/cmp-nvim-lsp" }
+    use { "hrsh7th/cmp-nvim-lua" }
+
+    -- snippets
+    use { "L3MON4D3/LuaSnip" } --snippet engine
+    use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
+
 
     -- LSP
     -- simple to use language server installer
@@ -93,20 +95,22 @@ return require("packer").startup(function(use)
     -- Enable lspconfig
     use { "neovim/nvim-lspconfig" }
     use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
-    use {
-        "RRethy/vim-illuminate",
-        config = get_config("illuminate")
-    }
+    use { "RRethy/vim-illuminate" }
+
+
+    -- Telescope
+    use { "nvim-telescope/telescope.nvim" }
+
+    -- Highlighting
+    -- Treesitter
+    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+    use { "nvim-treesitter/nvim-treesitter-refactor" }
+    use { "nvim-treesitter/nvim-treesitter-textobjects" }
 
     -- DAP
-    use {
-        "mfussenegger/nvim-dap",
-        requires = {
-            "rcarriga/nvim-dap-ui",
-            "ravenxrz/DAPInstall.nvim",
-        },
-        config = get_config("dap"),
-    }
+    use { "mfussenegger/nvim-dap" }
+    use { "rcarriga/nvim-dap-ui" }
+    use { "ravenxrz/DAPInstall.nvim" }
 
     -- Java
     use { "mfussenegger/nvim-jdtls" }
@@ -118,3 +122,4 @@ return require("packer").startup(function(use)
         require("packer").sync()
     end
 end)
+
