690374ebcc83eb45be1cd6bbaf2fb0ca27800059
S W Kim <ionia.kim@gmail.com>
2022-08-21 23:32:20 +0900

    Set whichkey settings

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,10 +1,3 @@
 require "nvim-tree".setup{
 }
 
--- keymaps
-local map = vim.keymap.set
-
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { noremap = true })
-map("n", "<leader>r", "<cmd>NvimTreeRefresh<CR>", { noremap = true })
-map("n", "<leader>n", "<cmd>NvimTreeFindFile<CR>", { noremap = true })
-
--- nvim/lua/config/whichkey.lua
+++ nvim/lua/config/whichkey.lua
@@ -26,7 +26,7 @@ local setup = {
   },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
-  operators = { gc = "Comments" },
+  -- operators = { gc = "Comments" },
   key_labels = {
     -- override the label used to display some keys. It doesn't effect WK in any other way.
     -- For example:
@@ -70,4 +70,106 @@ local setup = {
   },
 }
 
+local opts = {
+    mode = "n", -- NORMAL mode
+    prefix = "<leader>",
+    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true, -- use 'silent' when creating keymaps
+    noremap = true, -- use 'noremap' when creating keymaps
+    nowait = true, -- use 'nowait' when creating keymaps
+}
+
+local mappings = {
+    ["a"] = { "<cmd>Alpha<cr>", "Alpha" },
+    ["b"] = { "<cmd>Telescope buffers<cr>", "Buffers", },
+    -- ["c"] = { "<cmd>Bdelete!<cr>", "Close Buffer" },
+    ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
+    ["h"] = { "<cmd>nohlsearch<cr>", "No Highlight" },
+    ["v"] = { "<cmd>vsplit<cr>", "vsplit" },
+    ["s"] = { "<cmd>split<cr>", "split" },
+
+
+    P = {
+        name = "Packer",
+        c = { "<cmd>PackerCompile<cr>", "Compile" },
+        i = { "<cmd>PackerInstall<cr>", "Install" },
+        s = { "<cmd>PackerSync<cr>", "Sync" },
+        S = { "<cmd>PackerStatus<cr>", "Status" },
+        u = { "<cmd>PackerUpdate<cr>", "Update" },
+    },
+
+    o = {
+        name = "Options",
+        c = { '<cmd>lua vim.g.cmp_active=false<cr>', "Completion off" },
+        C = { '<cmd>lua vim.g.cmp_active=true<cr>', "Completion on" },
+    },
+
+    f = {
+        name = "Find",
+        b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+        c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+        f = { "<cmd>Telescope find_files<cr>", "Find files" },
+        t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+        s = { "<cmd>Telescope grep_string<cr>", "Find String" },
+        h = { "<cmd>Telescope help_tags<cr>", "Help" },
+        H = { "<cmd>Telescope highlights<cr>", "Highlights" },
+        -- i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
+        l = { "<cmd>Telescope resume<cr>", "Last Search" },
+        M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+        R = { "<cmd>Telescope registers<cr>", "Registers" },
+        k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+        C = { "<cmd>Telescope commands<cr>", "Commands" },
+    },
+
+    l = {
+        name = "LSP",
+        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+        -- c = { "<cmd>lua require('lsp').server_capabilities()<cr>", "Get Capabilities" },
+        -- d = {
+        --     "<cmd>Telescope lsp_document_diagnostics<cr>",
+        --     "Document Diagnostics",
+        -- },
+        -- w = {
+        --     "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+        --     "Workspace Diagnostics",
+        -- },
+        f = { "<cmd>lua vim.lsp.buf.format({async=true})<cr>", "Format" },
+        i = { "<cmd>LspInfo<cr>", "Info" },
+        I = { "<cmd>Mason<cr>", "Installer Info" },
+        j = {
+            "<cmd>lua vim.diagnostic.goto_next()<CR>",
+            "Next Diagnostic",
+        },
+        k = {
+            "<cmd>lua vim.diagnostic.goto_prev()<cr>",
+            "Prev Diagnostic",
+        },
+        l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+        q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+        r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+        s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+        S = {
+            "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+            "Workspace Symbols",
+        },
+    },
+
+    t = {
+        name = "Terminal",
+        ["1"] = { ":1ToggleTerm<cr>", "1" },
+        ["2"] = { ":2ToggleTerm<cr>", "2" },
+        ["3"] = { ":3ToggleTerm<cr>", "3" },
+        ["4"] = { ":4ToggleTerm<cr>", "4" },
+        -- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+        -- u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
+        -- t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+        -- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+        f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+        h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+        v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+    },
+}
+
 which_key.setup(setup)
+which_key.register(mappings, opts)
