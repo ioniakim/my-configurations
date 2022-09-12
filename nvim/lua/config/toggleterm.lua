d6ad57fb7ba714879018bdf7c25e0463c7b160ef
S W Kim <ionia.kim@gmail.com>
2022-09-12 10:36:38 +0900

    Change keymaps for moving around windows for toggleterm

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -34,13 +34,17 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  -- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true, buffer = 0 }
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    vim.keymap.set("t", "<C-h>", [[<cmd>wincmd h<cr>]], opts)  -- wincmd: equals to <C-w> window command prefix, useful when a Normal mode cannot be used directly (eg. this case) 
+    vim.keymap.set("t", "<C-j>", [[<cmd>wincmd j<cr>]], opts)
+    vim.keymap.set("t", "<C-k>", [[<cmd>wincmd k<cr>]], opts)
+    vim.keymap.set("t", "<C-l>", [[<cmd>wincmd l<cr>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -57,7 +61,7 @@ local lazygit = Terminal:new({
     on_open = function(term)
         -- start INSERT-MODE or TERMINAL-MODE in a terminal buffer
         vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<cr>", {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<cr>", { noremap = true, silent = true })
     end,
     on_close = function(term)
     end,
