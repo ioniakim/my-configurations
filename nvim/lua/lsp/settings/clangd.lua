8971996141721d9c70aee2898a6d12e754c2fb37
S W Kim <ionia.kim@gmail.com>
2022-09-13 22:23:25 +0900

    Delete deprecated and useless options for clangd lsp

--- nvim/lua/lsp/settings/clangd.lua
+++ nvim/lua/lsp/settings/clangd.lua
@@ -4,19 +4,9 @@ return {
         "--background-index",
         "--pch-storage=memory",
         "--clang-tidy",
-        "--suggest-missing-includes",
-        "--cross-file-rename",
         "--completion-style=detailed",
         "--header-insertion-decorators",
         "--header-insertion=iwyu"
     },
     filetype = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-    -- root_dir = {
-    -- },
-    init_options = {
-        clangdFileStatus = true,
-        usePlaceholders = true,
-        completeUnimported = true,
-        -- semanticHighlighting = true,
-    },
 }
