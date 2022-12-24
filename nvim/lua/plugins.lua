a02ecd4538bf238c966a3f65a19781e81c21df40
S W Kim <ionia.kim@gmail.com>
2022-12-24 19:49:13 +0900

    Set neovim plnanery plugin version to v0.1.0

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -45,7 +45,7 @@ packer.init {
 
 return packer.startup(function(use)
     use { "wbthomason/packer.nvim" } -- Have packer manage itself
-    use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
+    use { "nvim-lua/plenary.nvim", tag = "v0.1.0"} -- Useful lua functions used by lots of plugins
     use { "nvim-lua/popup.nvim" }
 
     -- Comment out
