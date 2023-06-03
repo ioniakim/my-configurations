6e46008f2a6591b5405b4564d24525e8624c8eaa
S W Kim <ionia.kim@gmail.com>
2023-06-03 17:47:35 +0900

    modify: disable treesitter textobjects and commentstring for error

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -131,8 +131,8 @@ return packer.startup(function(use)
     -- Treesitter
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
     -- use { "nvim-treesitter/nvim-treesitter-refactor" }
-    use { "nvim-treesitter/nvim-treesitter-textobjects" }
-    use { "JoosepAlviste/nvim-ts-context-commentstring" }
+    -- use { "nvim-treesitter/nvim-treesitter-textobjects" }
+    -- use { "JoosepAlviste/nvim-ts-context-commentstring" }
 
     -- DAP
     use { "mfussenegger/nvim-dap" }
