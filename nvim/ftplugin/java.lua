04da4076bfa3c97c401523da4d7859fc8e4f57bc
S W Kim <ionia.kim@gmail.com>
2023-06-03 21:36:43 +0900

    fix: cmp_nvim_lsp's deprecated method

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -8,7 +8,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if status_cmp_ok then
     capabilities.textDocument.completion.completionItem.snippetSupport = false
-    capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
 
 -- local jdtls_dir = vim.fn.expand("~/.local/share/nvim/lsp_servers/jdtls")
