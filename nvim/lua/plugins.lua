476e2a2f03091de87f247933ee4d9c4a5032b959
S W Kim <ionia.kim@gmail.com>
2023-06-03 17:50:47 +0900

    modify: fix causing Packer to fail upon the first installation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -129,7 +129,13 @@ return packer.startup(function(use)
 
     -- Highlighting
     -- Treesitter
-    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+    use { 
+        "nvim-treesitter/nvim-treesitter", 
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    }
     -- use { "nvim-treesitter/nvim-treesitter-refactor" }
     -- use { "nvim-treesitter/nvim-treesitter-textobjects" }
     -- use { "JoosepAlviste/nvim-ts-context-commentstring" }
