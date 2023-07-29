de0611e793b8cf50d7edd01687773191e3204fd7
Sung Woo Kim <ionia.kim@gmail.com>
2023-07-29 14:19:09 +0900

    delete: linux command toggleterm unable in windows

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -36,11 +36,6 @@ toggleterm.setup({
 function _G.set_terminal_keymaps()
     local opts = { noremap = true, buffer = 0 }
     -- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    -- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
     vim.keymap.set("t", "<C-h>", [[<cmd>wincmd h<cr>]], opts) -- wincmd: equals to <C-w> window command prefix, useful when a Normal mode cannot be used directly (eg. this case)
     vim.keymap.set("t", "<C-j>", [[<cmd>wincmd j<cr>]], opts)
     vim.keymap.set("t", "<C-k>", [[<cmd>wincmd k<cr>]], opts)
@@ -50,30 +45,3 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-
-local Terminal = require("toggleterm.terminal").Terminal
-
-local lazygit = Terminal:new({
-    cmd = "lazygit",
-    hidden = true,
-    direction = "float",
-    close_on_exit = true,
-    on_open = function(term)
-        -- start INSERT-MODE or TERMINAL-MODE in a terminal buffer
-        vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<cr>", { noremap = true, silent = true })
-    end,
-    on_close = function(term)
-    end,
-    count = 99,
-})
-
-function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-    htop:toggle()
-end
