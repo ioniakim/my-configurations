ee9b956c8dcb85a42c3e62f093a5f1fda09e2478
S W Kim <ionia.kim@gmail.com>
2025-02-01 21:55:23 +0900

    modify: nvim tab lengh to 2

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -30,9 +30,9 @@ set.infercase = true
 
 -- indentation
 vim.cmd('filetype plugin indent on')
-set.tabstop = 4
-set.shiftwidth = 4
-set.softtabstop = 4
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
 set.expandtab = true
 set.autoindent = true
 
